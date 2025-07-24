2df223e3c42a20b5fb92a59e85fd4c9d4ebf0727
RISHINAN56 <73730088+RISHINAN56@users.noreply.github.com>
2025-07-24 18:27:08 +0530

    Improve

--- Mock/Role/urls.py
+++ Mock/Role/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.role_selection, name='role_selection'),
+    path('frontend/', views.frontend, name='frontend'),
+    path('backend/', views.backend_view, name='backend'),
+    path('aptitude/', views.aptitude_view, name='aptitude'),
+    path('quiz/', views.start_quiz, name='start_quiz'),
+    path('submit-quiz/', views.submit_quiz, name='submit_quiz'),
+    path('aptitude-quiz/', views.start_quiz),
+
+
 ]
--- Mock/Role/views.py
+++ Mock/Role/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
+def start_quiz(request):
+    return render(request, 'quiz.html')
+
+def submit_quiz(request):
+    if request.method == 'POST':
+        score = 0
+        answers = {
+            'q1': '32',
+            'q2': '24',
+            'q3': '12s',
+            'q4': '9',
+            'q5': 'Thursday',
+        }
+        for key in answers:
+            if request.POST.get(key) == answers[key]:
+                score += 1
+        return render(request, 'Role/quiz_result.html', {'score': score, 'total': len(answers)})
+    return render(request, 'quiz.html')
+
+
 
 def role_selection(request):
     return render(request, 'Role/role_selection.html')
+
+def frontend(request):
+    return render(request, 'Role/frontend.html')
+def backend_view(request):
+    return render(request, 'Role/backend.html')
+
+def aptitude_view(request):
+    return render(request, 'Role/aptitude.html')
+
 
 
 # Create your views here.
